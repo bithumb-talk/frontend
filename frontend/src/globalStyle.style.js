aa50d7ed620c43bf59775cb57d7802e422835dbc
kimjanghu <kjh921030@gmail.com>
2021-09-24 00:08:07 +0900

    feat: global style 추가

---
+++ frontend/src/globalStyle.style.js
@@ -0,0 +1,79 @@
+import { Global, css } from '@emotion/react';
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      * {
+        box-sizing: border-box;
+        scroll-behavior: smooth;
+      }
+
+      html,
+      body,
+      #root {
+        width: 100%;
+        height: 100%;
+        font-size: 16px;
+      }
+
+      html,
+      body,
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6,
+      p,
+      a,
+      img,
+      ol,
+      ul,
+      li,
+      fieldset,
+      form,
+      label,
+      legend,
+      article,
+      footer,
+      header,
+      nav,
+      section {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+      }
+
+      article,
+      footer,
+      header,
+      nav,
+      section {
+        display: block;
+      }
+
+      body {
+        line-height: 1;
+      }
+
+      ol,
+      ul {
+        list-style: none;
+      }
+
+      a,
+      a:link,
+      a:visited,
+      a:hover,
+      a:active {
+        color: inherit;
+        text-decoration: none;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
