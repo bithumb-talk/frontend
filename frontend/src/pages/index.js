ebfe3cdcc3f4985ec3f77a36bdea79b88462dcd4
kimjanghu <kjh921030@gmail.com>
2021-09-27 09:43:35 +0900

    feat: 코인 시세 및 차트 페이지 레이아웃 구현

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as HomeMainPage } from './HomeMainPage/HomeMainPage';
 export { default as LoginPage } from './LoginPage/LoginPage';
 export { default as MyPage } from './MyPage/MyPage';
 export { default as NotFound } from './NotFound/NotFound';
+export { default as CoinInfoPage } from './CoinInfoPage/CoinInfoPage';
