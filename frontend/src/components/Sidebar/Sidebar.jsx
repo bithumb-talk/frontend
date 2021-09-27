6df0872121828e6fba9a610fe40d2946810d3aaa
kimjanghu <kjh921030@gmail.com>
2021-09-27 09:43:56 +0900

    feat: 코인 실시간 리스트 차트 사이드바 구현

---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SidebarContainer = styled.section`
+  display: flex;
+  position: fixed;
+  top: 0;
+  right: 0;
+  transform: ${({ position }) => `translateX(${position}px)`};
+  transition: 0.3s ease;
+`;
+
+const ToggleButton = styled.button`
+  width: 98px;
+  padding: 0;
+  margin: 0;
+  top: 50%;
+  position: fixed;
+  left: -74px;
+  transform: rotate(-90deg);
+  visibility: ${({ isSidebar }) => (isSidebar ? 'hidden' : 'visible')};
+`;
+
+function Sidebar({ children, width }) {
+  const [position, setPosition] = useState(width);
+  const [isSidebar, setIsSidebar] = useState(false);
+
+  const toggleMenu = () => {
+    if (position > 0) {
+      setPosition(0);
+      setIsSidebar(true);
+      return;
+    }
+    setPosition(width);
+    setIsSidebar(false);
+  };
+
+  return (
+    <SidebarContainer
+      position={position}
+    >
+      <ToggleButton
+        type="button"
+        onMouseEnter={() => toggleMenu()}
+        isSidebar={isSidebar}
+      >
+        CoinPrice
+      </ToggleButton>
+      <div
+        onMouseLeave={() => toggleMenu()}
+      >
+        {children}
+      </div>
+    </SidebarContainer>
+  );
+}
+
+Sidebar.propTypes = {
+  children: PropTypes.element.isRequired,
+  width: PropTypes.number.isRequired,
+};
+
+export default Sidebar;
