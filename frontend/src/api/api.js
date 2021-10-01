48deafb51c72a5e60ecbc3702a40e90fe63ba49f
KimJanghu <kjh921030@gmail.com>
2021-10-01 16:40:59 +0900

    feat: getCoinList api 추가

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,6 +4,7 @@ const BASE_URL = '';
 
 const END_POINT = Object.freeze({
   INTEREST: `${BASE_URL}/interest`,
+  COIN: `${BASE_URL}/quote_init`,
 });
 
 class Api {
@@ -13,6 +14,11 @@ class Api {
 
   async getInterest(userId) {
     const res = await this.api.get(`${END_POINT.INTEREST}/${userId}`);
+    return res;
+  }
+
+  async getCoinList() {
+    const res = await this.api.get(`${END_POINT.COIN}`);
     return res;
   }
 }
