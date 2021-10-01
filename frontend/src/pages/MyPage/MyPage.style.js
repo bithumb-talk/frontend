9cf211041dde7cf2cc605bc24b1bc2f24ac383a9
이솔 <sol6252@naver.com>
2021-10-02 05:59:22 +0900

    feat: mypage 페이지 구현 중

--- frontend/src/pages/MyPage/MyPage.style.js
+++ frontend/src/pages/MyPage/MyPage.style.js
@@ -1,52 +1,22 @@
-import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Fab from '@mui/material/Fab';
+import {
+  Avatar, Box, Typography, TextField, Button,
+} from '@mui/material';
 import { styled } from '@mui/material/styles';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { stringAvatar } from '../../utils/index';
 
-export const UserProfile = styled((props) => <Avatar {...stringAvatar(props.nickname)} />)({});
+export const UserProfileBox = styled((props) => <Box {...props} />)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 export const UserInfo = styled((props) => <Box {...props} />)({
   display: 'flex',
   flexDirection: 'column',
   marginLeft: '10px',
   justifyContent: 'center',
-  alignItems: 'center',
-});
-
-export const UserNickname = styled((props) => <Typography {...props} />)({
-  fontSize: 18,
-});
-
-export const LoginButton = styled((props) => <Button {...props} />)({
-  fontSize: '1rem',
   minWidth: '120px',
-});
-
-export const Notifications = styled((props) => <NotificationsIcon {...props} />)({
-  fontSize: '1.2rem',
   width: '25px',
-  height: '25px',
-});
-
-export const NotificationsNone = styled((props) => <NotificationsNoneIcon {...props} />)({
-  fontSize: '1.2rem',
-  width: '25px',
-  height: '25px',
-});
-
-export const ArrowDropDown = styled((props) => <ArrowDropDownIcon {...props} />)({
-  fontSize: '1.2rem',
-  width: '38px',
-  height: '38px',
-});
-
-export const IconWrap = styled((props) => <Fab {...props} />)({
   margin: '3.5px',
 });
 
@@ -56,4 +26,54 @@ export const UserIcons = styled((props) => <Box {...props} />)({
   marginLeft: '10px',
   alignItems: 'center',
   justifyContent: 'center',
+});
+
+export const ProfileImage = styled((props) => <Avatar {...props} />)({});
+
+export const NicknameBox = styled((props) => <Box {...props} />)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '10px',
+  width: '300px',
+});
+
+export const UserNickname = styled((props) => <TextField size="small" variant="standard" {...props} />)({
+  fontSize: '1.5rem',
+  marginRight: '10px',
+});
+
+export const ChangeButton = styled((props) => <Button variant="contained" size="small" {...props} />)({});
+
+export const PWChangeButton = styled((props) => (
+  <Button
+    variant="contained"
+    size="medium"
+    color="success"
+    width="100%"
+    {...props}
+  />
+))({});
+
+export const UserContentBox = styled((props) => <Box {...props} />)({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '20px',
+  width: '100%',
+});
+
+export const ContentWrap = styled((props) => <Box {...props} />)({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '20px',
+  width: '100%',
+});
+
+export const ContentTitle = styled((props) => <Typography {...props} />)({ fontSize: '1.2rem' });
+
+export const DivideLine = styled((props) => <hr {...props} />)({
+  color: 'black',
+  marginTop: '30px',
+  marginBottom: '30px',
+  width: '100%',
 });
