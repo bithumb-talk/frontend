cf65d921d4015deee71532c01c16cd289838dda9
unknown <ajgkwl0125@naver.com>
2021-10-08 20:27:33 +0900

    feat: 댓글 좋아요 api 연결

--- frontend/src/components/Board/CommentView.jsx
+++ frontend/src/components/Board/CommentView.jsx
@@ -1,17 +1,38 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable react/jsx-key */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import proptypes from 'prop-types';
+import api from '@/api/api';
 import { CommentLikeIcon, CommentLikeEmptyIcon } from './Board.style';
 import './BoardDetail.style.css';
 
 export default function CommentView(props) {
   const { item } = props;
   const [check, setCheck] = useState(false);
+  const [boardNo, setNo] = useState('');
 
-  const onClickComment = () => {
+  const onClickComment = async () => {
     setCheck(!check);
+    const data = {
+      commentRecommend: null,
+    };
+    if (check) {
+      data.commentRecommend = 'false';
+    } else {
+      data.commentRecommend = 'true';
+    }
+    await api.postCommentRecommend(boardNo, item.commentNo, data).then((res) => {
+      if (res.data.status === 'SUCCESS') {
+        alert('저장 성공');
+      } else {
+        alert('저장 실패');
+      }
+    });
   };
+
+  useEffect(() => {
+    if (item && item.links) {
+      setNo(item.links[0].href.split('/')[4]);
+    }
+  }, [item]);
 
   return (
     <div>
