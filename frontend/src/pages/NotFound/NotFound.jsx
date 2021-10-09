ce7f0f2eb2693ff2d33b82cb005c060b30e8541b
unknown <ajgkwl0125@naver.com>
2021-10-09 23:10:48 +0900

    feat: Not Found 구현

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,28 @@
-const NotFound = () => <>페이지를 찾지 못했습니다.</>;
+import { Link } from 'react-router-dom';
+import orange404 from '@/assets/image/orange404.png';
+
+const NotFound = () => (
+  <div style={{ textAlign: 'center', margin: '5em' }}>
+    <img src={orange404} alt="404" width="400" height="300" />
+    <div style={{ fontSize: '3em', margin: '1rem' }}>아무것도 없어요</div>
+    <Link to="/" style={{ display: 'flex', justifyContent: 'center' }}>
+      <div
+        style={{
+          background: '#f90',
+          color: 'white',
+          fontSize: '1.2em',
+          width: '150px',
+          height: '30px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          cursor: 'pointer',
+        }}
+      >
+        홈으로
+      </div>
+    </Link>
+  </div>
+);
 
 export default NotFound;
