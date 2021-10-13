993215f8379e5ef82bb1d6289cf81259c7bff26d
KimJanghu <kjh921030@gmail.com>
2021-10-13 16:46:52 +0900

    fix: reset option

--- frontend/src/utils/setCustomChartOptions.js
+++ frontend/src/utils/setCustomChartOptions.js
@@ -1,188 +1,188 @@
 import { COLOR } from '@/constants/style';
-// import { COIN_CHART_GAP, DEFAULT_ZOOM_OPTION } from '@/constants/coin';
+import { COIN_CHART_GAP, DEFAULT_ZOOM_OPTION } from '@/constants/coin';
 
-// const setZoomButtons = (gap) => {
-//   switch (gap) {
-//     case COIN_CHART_GAP.TEN.UNIT:
-//       return [
-//         ...DEFAULT_ZOOM_OPTION,
-//         {
-//           type: 'day',
-//           count: 1,
-//           text: '1day',
-//           title: 'View 1 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [10]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 2,
-//           text: '2day',
-//           title: 'View 2 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [10]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 3,
-//           text: '3day',
-//           title: 'View 3 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [10]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 6,
-//           text: '6day',
-//           title: 'View 6 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [10]]],
-//           },
-//         },
-//       ];
-//     case COIN_CHART_GAP.THIRTY.UNIT:
-//       return [
-//         ...DEFAULT_ZOOM_OPTION,
-//         {
-//           type: 'day',
-//           count: 1,
-//           text: '1day',
-//           title: 'View 1 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [30]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 2,
-//           text: '2day',
-//           title: 'View 2 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [30]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 3,
-//           text: '3day',
-//           title: 'View 3 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [30]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 6,
-//           text: '6day',
-//           title: 'View 6 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['minute', [30]]],
-//           },
-//         },
-//       ];
-//     case COIN_CHART_GAP.HOUR.UNIT:
-//       return [
-//         ...DEFAULT_ZOOM_OPTION,
-//         {
-//           type: 'day',
-//           count: 3,
-//           text: '3day',
-//           title: 'View 3 day',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['hour', [1]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 7,
-//           text: '1 weeks',
-//           title: 'View 1 weeks',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['hour', [1]]],
-//           },
-//         },
-//         {
-//           type: 'day',
-//           count: 14,
-//           text: '2 weeks',
-//           title: 'View 2 weeks',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['hour', [1]]],
-//           },
-//         },
-//         {
-//           type: 'month',
-//           count: 1,
-//           text: '1mon',
-//           title: 'View 1 mon',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['hour', [1]]],
-//           },
-//         },
-//       ];
-//     default:
-//       return [
-//         ...DEFAULT_ZOOM_OPTION,
-//         {
-//           type: 'month',
-//           count: 1,
-//           text: '1mon',
-//           title: 'View 1 month',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['day', [1]]],
-//           },
-//         },
-//         {
-//           type: 'month',
-//           count: 2,
-//           text: '2mon',
-//           title: 'View 2 month',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['day', [1]]],
-//           },
-//         },
-//         {
-//           type: 'month',
-//           count: 3,
-//           text: '3mon',
-//           title: 'View 3 month',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['day', [1]]],
-//           },
-//         },
-//         {
-//           type: 'month',
-//           count: 6,
-//           text: '6mon',
-//           title: 'View 6 month',
-//           dataGrouping: {
-//             forced: true,
-//             units: [['day', [1]]],
-//           },
-//         },
-//       ];
-//   }
-// };
+const setZoomButtons = (gap) => {
+  switch (gap) {
+    case COIN_CHART_GAP.TEN.UNIT:
+      return [
+        ...DEFAULT_ZOOM_OPTION,
+        {
+          type: 'day',
+          count: 1,
+          text: '1day',
+          title: 'View 1 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [10]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 2,
+          text: '2day',
+          title: 'View 2 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [10]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 3,
+          text: '3day',
+          title: 'View 3 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [10]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 6,
+          text: '6day',
+          title: 'View 6 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [10]]],
+          },
+        },
+      ];
+    case COIN_CHART_GAP.THIRTY.UNIT:
+      return [
+        ...DEFAULT_ZOOM_OPTION,
+        {
+          type: 'day',
+          count: 1,
+          text: '1day',
+          title: 'View 1 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [30]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 2,
+          text: '2day',
+          title: 'View 2 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [30]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 3,
+          text: '3day',
+          title: 'View 3 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [30]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 6,
+          text: '6day',
+          title: 'View 6 day',
+          dataGrouping: {
+            forced: true,
+            units: [['minute', [30]]],
+          },
+        },
+      ];
+    case COIN_CHART_GAP.HOUR.UNIT:
+      return [
+        ...DEFAULT_ZOOM_OPTION,
+        {
+          type: 'day',
+          count: 3,
+          text: '3day',
+          title: 'View 3 day',
+          dataGrouping: {
+            forced: true,
+            units: [['hour', [1]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 7,
+          text: '1 weeks',
+          title: 'View 1 weeks',
+          dataGrouping: {
+            forced: true,
+            units: [['hour', [1]]],
+          },
+        },
+        {
+          type: 'day',
+          count: 14,
+          text: '2 weeks',
+          title: 'View 2 weeks',
+          dataGrouping: {
+            forced: true,
+            units: [['hour', [1]]],
+          },
+        },
+        {
+          type: 'month',
+          count: 1,
+          text: '1mon',
+          title: 'View 1 mon',
+          dataGrouping: {
+            forced: true,
+            units: [['hour', [1]]],
+          },
+        },
+      ];
+    default:
+      return [
+        ...DEFAULT_ZOOM_OPTION,
+        {
+          type: 'month',
+          count: 1,
+          text: '1mon',
+          title: 'View 1 month',
+          dataGrouping: {
+            forced: true,
+            units: [['day', [1]]],
+          },
+        },
+        {
+          type: 'month',
+          count: 2,
+          text: '2mon',
+          title: 'View 2 month',
+          dataGrouping: {
+            forced: true,
+            units: [['day', [1]]],
+          },
+        },
+        {
+          type: 'month',
+          count: 3,
+          text: '3mon',
+          title: 'View 3 month',
+          dataGrouping: {
+            forced: true,
+            units: [['day', [1]]],
+          },
+        },
+        {
+          type: 'month',
+          count: 6,
+          text: '6mon',
+          title: 'View 6 month',
+          dataGrouping: {
+            forced: true,
+            units: [['day', [1]]],
+          },
+        },
+      ];
+  }
+};
 
-const setCustomChartOptions = (data) => ({
+const setCustomChartOptions = (data, gap) => ({
   yAxis: [
     {
       labels: {
@@ -244,12 +244,12 @@ const setCustomChartOptions = (data) => ({
     zoomType: 'x',
   },
   rangeSelector: {
-    enabled: false,
-    // selected: 1,
-    // buttons: setZoomButtons(gap),
-    // buttonTheme: {
-    //   width: 40,
-    // },
+    // enabled: false,
+    selected: 1,
+    buttons: setZoomButtons(gap),
+    buttonTheme: {
+      width: 40,
+    },
   },
   tooltip: {
     // formatter() {
